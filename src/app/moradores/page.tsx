4d1d91b04189f72891d2990023d233b9120d93ed
calouros <calouros@fatec.corp>
2025-08-22 22:16:31 -0300

    feat: moradores page

---
+++ src/app/moradores/page.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+export default function ListaMoradores(){
+    return(
+        <div className="p-6 max-w-full">
+            <div className="mb-4 flex items-center justify-between gap-4">
+                <h1 className="text-xl font-semibold">Moradores</h1>
+            </div>
+
+            <div className="bg-white rounded-md border border-gray-200 overflow-hidden">
+                <table className="min-w-full devide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                        <tr>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">#</th>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">Nome</th>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">Contato</th>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">Condomínio</th>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">Unidade</th>
+                            <th className="px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-winder w-12">Ação</th>
+                        </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                        <tr>
+                            <td className="px-4 py-3 text-sm text-gray-700" colSpan={7}>
+                            Nenhum morador encontrado.
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
